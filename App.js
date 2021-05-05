c10f21ca0e54888841af2d1d8d6bb01ccb03cfb0
Masih Shekarak <64202207+Masih17@users.noreply.github.com>
2021-05-05 23:18:11 +0300

    Styling of the main page

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import { StatusBar } from 'react-native';
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
-import CoinList from './components/CoinList';
+import { StatusBar } from "react-native";
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+import CoinList from "./components/CoinList";
 
-const THEME_COLOR = 'black';
+const THEME_COLOR = "black";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.topSafeArea} />
       <SafeAreaView style={styles.bottomSafeArea}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <CoinList />
       </SafeAreaView>
     </>
@@ -20,15 +20,6 @@ export default function App() {
 ///////////////////////////////////////////
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'red',
-    fontSize: 20,
-  },
   topSafeArea: {
     flex: 0,
     backgroundColor: THEME_COLOR,
--- components/CoinList.js
+++ components/CoinList.js
@@ -1,38 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 import {
   ActivityIndicator,
   StatusBar,
-  Animated,
   StyleSheet,
   Text,
   View,
   FlatList,
   Image,
   Dimensions,
-  SafeAreaViewBase,
   TouchableOpacity,
-  Easing,
   TextInput,
-} from 'react-native';
+  RefreshControl,
+} from "react-native";
+// import { LinearGradient } from 'expo-linear-gradient';
 
-import filter from 'lodash.filter';
-import axios from 'axios';
+import filter from "lodash.filter";
+import axios from "axios";
 
-const { width, height } = Dimensions.get('screen');
+const { width, height } = Dimensions.get("screen");
 const SPACING = 20;
 const ICON_SIZE = 40;
 
 function CoinList() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState();
   const [fullData, setFullData] = useState([]);
 
   const API_URI =
-    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=1000&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d';
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=5000&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d";
 
-  //coin list from CoinGecko
-  useEffect(() => {
+  const fetchData = () => {
     setIsLoading(true);
     axios
       .get(API_URI)
@@ -43,11 +41,16 @@ function CoinList() {
       })
       .catch((error) => {
         setIsLoading(false);
-        console.log('Error in axios', error);
+        console.log("Error in axios", error);
       });
+  };
+
+  //coin list from CoinGecko
+  useEffect(() => {
+    fetchData();
   }, []);
 
-  // To handle the search bar.
+  // To handle the searches
   const handleSearch = (text) => {
     const formattedQuery = text.toLowerCase();
     const filteredData = filter(fullData, (symbol) => {
@@ -57,6 +60,7 @@ function CoinList() {
     setQuery(text);
   };
 
+  // to filter the search by these parameter of the data in the API
   const contains = ({ symbol, id, name }, query) => {
     if (symbol.includes(query) || id.includes(query) || name.includes(query)) {
       return true;
@@ -64,44 +68,73 @@ function CoinList() {
     return false;
   };
 
+  /////////////////////////////// VIEW ////////////////////////////////
+
   return (
-    <View style={styles.container}>
-      <TextInput
-        autoCapitalize='none'
-        autoCorrect={false}
-        clearButtonMode='always'
-        value={query}
-        onChangeText={handleSearch}
-        placeholder='Search'
-        style={styles.searchBox}
-      />
+    <View>
+      {/*//////////////// SearchBar ////////////// */}
+      <View>
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          clearButtonMode="always"
+          value={query}
+          onChangeText={handleSearch}
+          placeholder="Search"
+          placeholderTextColor="#fefefe"
+          style={styles.searchBox}
+        />
+      </View>
+      {/*//////////////// FlatList////////////// */}
       {isLoading ? (
-        <ActivityIndicator animating size='large' />
+        <ActivityIndicator animating size="small" />
       ) : (
         <FlatList
           data={coins}
+          initialNumToRender={15} //Not sure if this is working or not
           keyExtractor={(item) => item.id}
-          contentContainerStyle={{
-            padding: SPACING,
-            paddingTop: StatusBar.currentHeight || 42,
-          }}
-          renderItem={({ item, index }) => (
-            <TouchableOpacity onPress={() => alert('Item pressed!')}>
-              <View style={styles.flatListItems}>
+          ///////// Pull to refresh //////////
+          refreshControl={
+            <RefreshControl refreshing={isLoading} onRefresh={fetchData} />
+          }
+          style={styles.flatListBody}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => alert("Item pressed!")}>
+              <View style={styles.flatListBox}>
                 <Image
                   source={{
                     uri: item.image,
                   }}
-                  style={{
-                    width: styles.image.width,
-                    height: styles.image.height,
-                    borderRadius: styles.image.borderRadius,
-                    marginRight: styles.image.marginLeft,
-                  }}
+                  style={styles.icon}
                 />
-                <View>
-                  <Text style={styles.coinName}>{item.name}</Text>
-                  <Text style={styles.text}>{item.symbol.toUpperCase()}</Text>
+                <View style={styles.flatListContainerTexts}>
+                  {/******  Name and Symbol *********/}
+                  <View>
+                    <Text style={styles.coinTitle}>
+                      {item.id.charAt(0).toUpperCase() + item.id.slice(1)}
+                    </Text>
+                    <Text style={styles.coinSymbol}>
+                      {item.symbol.toUpperCase()}
+                    </Text>
+                  </View>
+                  {/******  Price and Percentage change in 24H *********/}
+                  <View style={styles.coinPriceBox}>
+                    <Text style={styles.price}>
+                      €{item.current_price.toFixed(2).replace(".", ",")}
+                    </Text>
+                    <Text
+                      // Checking if the percentage is up or down and change color base on that
+                      style={[
+                        item.price_change_percentage_24h_in_currency >= 0
+                          ? styles.percentageUp
+                          : styles.percentageDown,
+                      ]}
+                    >
+                      {item.price_change_percentage_24h_in_currency.toFixed(2) +
+                        "%"}
+                    </Text>
+                    {/* To the end of flex*/}
+                  </View>
                 </View>
               </View>
             </TouchableOpacity>
@@ -116,38 +149,78 @@ function CoinList() {
 ///////////////////////////////////////////
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-  },
-  flatListItems: {
-    flexDirection: 'row',
-    padding: SPACING,
-    marginBottom: SPACING / 2,
-    backgroundColor: 'rgba(59, 56, 74, 0.8)',
-    borderRadius: 12,
-  },
-  text: { color: 'white' },
-  coinName: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  image: {
-    // Crypto icon sizes
-    width: ICON_SIZE,
-    height: ICON_SIZE,
-    borderRadius: ICON_SIZE,
-    marginLeft: 10,
-  },
   searchBox: {
+    color: "white",
     padding: SPACING,
     marginBottom: SPACING / 4,
     margin: SPACING,
-    backgroundColor: '#fff',
+    backgroundColor: "rgba(59, 56, 74, 0.8)",
     borderRadius: 12,
-    height: 40,
-    borderColor: 'red',
-    borderWidth: 6,
-    color: 'red',
+    //height: 40, If height is set, the color of the font don't work.
+    //Like this the system takes care of it
+    marginBottom: 10,
+  },
+
+  flatListBody: {
+    padding: SPACING,
+    paddingTop: StatusBar.currentHeight || 24,
+  },
+  flatListBox: {
+    flexDirection: "row",
+    color: "#ffffff",
+    padding: SPACING,
+    paddingLeft: SPACING / 2,
+    paddingRight: SPACING / 2,
+    marginBottom: SPACING / 4,
+    backgroundColor: "rgba(43, 36, 79, 0.8)",
+    borderRadius: 12,
+    // borderColor: "yellow",
+    // borderWidth: 2,
+  },
+  flatListContainerTexts: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // borderWidth: 2,
+    // borderColor: "pink",
+  },
+  coinSymbol: {
+    color: "#ffffff",
+    // borderColor: "yellow",
+    // borderWidth: 2,
+  },
+  coinTitle: {
+    color: "#ffffff",
+    maxWidth: "100%",
+    fontWeight: "bold",
+    // borderColor: "yellow",
+    // borderWidth: 2,
+  },
+  icon: {
+    // Icon sizes
+    width: ICON_SIZE,
+    height: ICON_SIZE,
+    borderRadius: ICON_SIZE,
+    marginRight: 10,
+  },
+
+  coinPriceBox: {
+    color: "#ffffff",
+    // borderColor: "yellow",
+    // borderWidth: 2,
+    //alignSelf: "flex-start",
+  },
+  price: {
+    color: "#ffffff",
+    alignSelf: "flex-end",
+  },
+  percentageUp: {
+    color: "#45e600",
+    alignSelf: "flex-end",
+  },
+  percentageDown: {
+    color: "#c82a42",
+    alignSelf: "flex-end",
   },
 });
 
